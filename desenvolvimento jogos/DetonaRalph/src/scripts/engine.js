a8e735ad139b889af7b8dc97841d38f96f6570ff
Murilo Iachitzke <miachitzke@mail.com>
2024-01-17 09:43:47 -0300

    Pacote de melhorias
    - Adicionada a função startGame com as seguintes finalidades:
    Corrigir bug que impedia reprodução do som caso não tivesse interação prévia do usuário com a página; Esclarecer a mecânica do jogo para que o usuário saiba o que é considerado uma vitória e/ou derrota antes de iniciar a partida
    - Ajuste do tempo de partida
    - Remoção das actions para inicio manual partindo do user
    - Melhorias visuais.

--- desenvolvimento jogos/DetonaRalph/src/scripts/engine.js
+++ desenvolvimento jogos/DetonaRalph/src/scripts/engine.js
@@ -9,13 +9,11 @@ const state = {
   values: {
     hitPosition: 0,
     result: 0,
-    initialTime: 20,
-    currentTime: 20,
+    initialTime: 60,
+    currentTime: null,
     lives: 3,
-  },
-  action: {
-    timerId: setInterval(randomSquare, 1000),
-    countDownTimerId: setInterval(theFinalCountdown, 1000),
+    timerId: null,
+    countDownTimerId: null,
   },
 };
 
@@ -37,18 +35,27 @@ function theFinalCountdown() {
       );
       state.values.currentTime = state.values.initialTime;
     }
-    // Cenário 2: usuário tem mais de 10 pontos
-    else if (state.values.result > 10) {
+    // Cenário 2: usuário tem mais de 30 pontos
+    else if (state.values.result >= 30) {
       soundEffect("win");
-      clearInterval(state.action.timerId);
-      clearInterval(state.action.countDownTimerId);
+      clearInterval(state.values.timerId);
+      clearInterval(state.values.countDownTimerId);
       endGame(state.values.result);
     }
-    // Cenário 3: usuário está na última vida e tem menos de 10 pontos
-    else if (state.values.lives === 1) {
+    // Cenário 3: usuário tem menos de 30 pontos e mais de 1 vida
+    else if (state.values.result < 30 && state.values.lives > 1) {
+      livesChecker();
+      var plural = state.values.lives > 1 ? "chances!" : "chance!";
+      alert(
+        `Você não conseguiu acertar o Ralph! Mas ainda tem mais ${state.values.lives} ${plural}`
+      );
+      state.values.currentTime = state.values.initialTime;
+    }
+    // Cenário 4: usuário está na última vida e tem menos de 30 pontos
+    else if (state.values.lives === 1 && state.values.result < 30) {
       soundEffect("death");
-      clearInterval(state.action.timerId);
-      clearInterval(state.action.countDownTimerId);
+      clearInterval(state.values.timerId);
+      clearInterval(state.values.countDownTimerId);
       endGame(state.values.result);
     }
   }
@@ -80,20 +87,21 @@ function livesChecker() {
   if (state.values.lives <= 0) {
     state.view.lives.textContent = "x0";
     soundEffect("death");
-    clearInterval(state.action.timerId);
-    clearInterval(state.action.countDownTimerId);
+    clearInterval(state.values.timerId);
+    clearInterval(state.values.countDownTimerId);
     endGame(state.values.result);
   }
 }
 
 function endGame(score) {
-  var messageDiv = document.createElement("div");
-  messageDiv.id = "end-game";
+  var endMessage = document.createElement("div");
+  endMessage.id = "end-game";
+  endMessage.classList.add("center-content");
   var messageContent = document.createElement("p");
   messageContent.innerHTML =
-    `FIM DO JOGO!<br/>
+    `FIM DO JOGO!<br/><br/>
       Sua pontuação final foi: ` + score;
-  messageDiv.appendChild(messageContent);
+  endMessage.appendChild(messageContent);
 
   var playAgainBtn = document.createElement("button");
   playAgainBtn.textContent = "Jogar novamente";
@@ -101,9 +109,10 @@ function endGame(score) {
     window.location.reload();
   });
   playAgainBtn.style.marginTop = "15px";
-  messageDiv.appendChild(playAgainBtn);
+  playAgainBtn.style.padding = "1rem";
+  endMessage.appendChild(playAgainBtn);
 
-  document.body.appendChild(messageDiv);
+  document.body.appendChild(endMessage);
 }
 
 function addListenerHitBox() {
@@ -125,9 +134,46 @@ function addListenerHitBox() {
   });
 }
 
+function startGame() {
+  var instructions =
+    "Instruções:<br/>Acerte o Ralph o máximo de vezes que conseguir antes que o tempo acabe";
+  var rules =
+    "Regras:<br/>- O jogo inicia com 3 vidas. Você perde uma vida ao acertar uma caixa vazia ou quando o tempo se esgota e a pontuação é menor que 30.<br/>- Ao perder todas as vidas, o jogo encerra.<br/>- Você vencerá se sua pontuação total for maior que 30 quando o tempo se esgotar.";
+
+  var startButton = document.createElement("button");
+  startButton.textContent = "Iniciar Jogo";
+  startButton.addEventListener("click", () => {
+    init();
+    document.body.removeChild(startMessage);
+  });
+  startButton.style.marginTop = "15px";
+  startButton.style.padding = "1rem";
+
+  var startMessage = document.createElement("div");
+  startMessage.id = "start-game";
+  startMessage.classList.add("center-content");
+
+  var pInstructions = document.createElement("p");
+  pInstructions.innerHTML = instructions;
+  pInstructions.style.marginBottom = "15px";
+
+  var pRules = document.createElement("p");
+  pRules.innerHTML = rules;
+  pRules.style.marginBottom = "15px";
+
+  startMessage.appendChild(pInstructions);
+  startMessage.appendChild(pRules);
+  startMessage.appendChild(startButton);
+
+  document.body.appendChild(startMessage);
+}
+
 function init() {
-  document.body.click();
+  state.values.currentTime = state.values.initialTime;
+  state.values.timerId = setInterval(randomSquare, 1000);
+  state.values.countDownTimerId = setInterval(theFinalCountdown, 1000);
+
   addListenerHitBox();
 }
 
-init();
+startGame();
